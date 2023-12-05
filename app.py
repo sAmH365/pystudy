23c151acbb4eb01790da17747e4817f9536b3b2a
sAmH365 <npower5377@naver.com>
2023-12-05 22:11:21 +0900

    리스트, 딕셔너리 자료구조

--- app.py
+++ app.py
@@ -3,3 +3,18 @@ print('hello')
 someWord = '이런 문자가 있을 경우'
 
 print(someWord[0:4])  # 이런 문
+print()
+
+# 리스트, 딕셔너리 자료 구조
+중고차 = ['K5', 'whit', 5000]
+중고차[1] = 'black'
+
+중고차2 = { #딕셔너리 자료형(중괄호 사용)
+    'brand': 'BMW',
+    'model': '520d'
+}
+print(중고차[0])
+print(중고차2['brand'])
+
+중고차2['brand'] = 'abc123'
+print(중고차2['brand'])
