ac98032cf2edd475ac5a3aeb7acce0aa79795e3a
sAmH365 <npower5377@naver.com>
2024-03-23 16:07:16 +0900

    JSON 데이터 다루기2 : 데이터 출력과 시간

--- coinVal.py
+++ coinVal.py
@@ -1,13 +1,24 @@
 import requests
 import json
+import time
 
 data = requests.get(
     'https://api-gateway.coinone.co.kr/exchange/chart/v1/KRW/ETH?lastDt=1710575100000&interval=15m&1711114451118')
 # print(data.content)
 
 dict = json.loads(data.content)
-# print(dict['candles'][0])
 
-print(dict['body']['candles'][0]['close'])
-print(dict['body']['candles'][1]['close'])
-print(dict['body']['candles'][2]['close'])
+print(dict['body']['candles'][0])
+# print(dict['body']['candles'][0]['close'])
+# print(dict['body']['candles'][1]['close'])
+# print(dict['body']['candles'][2]['close'])
+
+dataList = dict['body']['candles']
+
+for i, v in enumerate(dataList):
+    print('========' + str(i) + '========')
+    formatTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(v['dt'] / 1000))
+    print('dt: ' + formatTime)
+    print('close: ' + v['close'])
+
+len(dict['body']['candles'])
